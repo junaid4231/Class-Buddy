e61571aa3f5763c4fb9e9df15bab186a3ac84367
Sajjad Afzaal <sajjadafzaal13416@gmail.com>
2025-06-01 16:09:15 +0500

    Notifiation cards added

---
+++ Components/NotificationCard.tsx
@@ -0,0 +1,50 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+interface NotificationCardProps {
+  text: string;
+  time: string;
+  category: string;
+}
+
+export default function NotificationCard({
+  text,
+  time,
+  category,
+}: NotificationCardProps) {
+  return (
+    <View style={styles.cardBody}>
+      <View style={styles.cardTxt}>
+        <Text numberOfLines={2}>{text}</Text>
+      </View>
+      <View style={styles.cardSection}>
+        <Text style={styles.updateTime}>{time}</Text>
+        <Text style={styles.category}>• {category}</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  cardBody: {
+    width: '100%',
+    height: 100,
+    flex: 1,
+    marginVertical: 8,
+  },
+  cardTxt: {
+    backgroundColor: 'white',
+    flex: 0.8,
+    padding: 10,
+    borderRadius: 8,
+    elevation: 5,
+  },
+  cardSection: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 8,
+  },
+  updateTime: {color: 'grey'},
+  category: {color: 'grey'},
+});
--- screens/NotificationsScreen.tsx
+++ screens/NotificationsScreen.tsx
@@ -1,20 +1,61 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, SafeAreaView, ScrollView} from 'react-native';
+import NotificationCard from '../Components/NotificationCard';
 
 const NotificationsScreen: React.FC = () => {
   return (
-    <View style={styles.container}>
-      <Text>Notifications Screen</Text>
-    </View>
+    <SafeAreaView>
+      <ScrollView style={styles.Container}>
+        <View style={styles.headingContainer}>
+          <Text style={styles.headingTxt}>Notifications</Text>
+          <Text style={styles.descriptionTxt}>
+            Find info related to ongoing and upcoming events and activities.
+          </Text>
+        </View>
+        <View style={styles.bodyContainer}>
+          <NotificationCard
+            text="Sports trial will be held in main cricket ground before football final match."
+            time="Updated 3hrs ago"
+            category="Sports"
+          />
+          <NotificationCard
+            text="Students are required to wear thier student cards while inside the
+                    university campus especially in labs."
+            time="Updated 4hrs ago"
+            category="Admin"
+          />
+          <NotificationCard
+            text="Live panel talk about freelancing in going to strat in seminar room A block."
+            time="Updated 2hrs ago"
+            category="Admin"
+          />
+          <NotificationCard
+            text="Submit the TOA assignment no 4 before 11:59pm on June 01, 2025."
+            time="Updated 4hrs ago"
+            category="TOA"
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
 export default NotificationsScreen;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  Container: {
+    margin: 16,
+    paddingVertical: 8,
+    paddingHorizontal: 6,
   },
+  headingContainer: {},
+  headingTxt: {
+    fontSize: 24,
+    color: '#1A6B7E',
+    fontWeight: 'bold',
+  },
+  descriptionTxt: {
+    marginVertical: 12,
+  },
+  bodyContainer: {},
 });
