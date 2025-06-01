663e7f38c019a274e02e23dc61ad6e4c57ed0c77
Sajjad Afzaal <sajjadafzaal13416@gmail.com>
2025-06-01 17:01:14 +0500

    Added bouncy checkbox and TodoCard created and used

---
+++ Components/TodoCard.tsx
@@ -0,0 +1,49 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import BouncyCheckBox from 'react-native-bouncy-checkbox';
+
+interface TodoCardProps {
+  text: string;
+  date: string;
+  category: string;
+}
+
+export default function TodoCard({text, date, category}: TodoCardProps) {
+  return (
+    <View style={styles.cardBody}>
+      <View style={styles.cardTxt}>
+        <BouncyCheckBox fillColor="#1A6B7E" />
+        <Text numberOfLines={1}>{text}</Text>
+      </View>
+      <View style={styles.cardSection}>
+        <Text style={styles.updateTime}>Due Date: {date}</Text>
+        <Text style={styles.category}>• {category}</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  cardBody: {
+    width: '100%',
+    height: 90,
+    flex: 1,
+    marginVertical: 8,
+  },
+  cardTxt: {
+    backgroundColor: 'white',
+    flex: 0.8,
+    padding: 10,
+    borderRadius: 8,
+    elevation: 5,
+    flexDirection: 'row',
+  },
+  cardSection: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 8,
+  },
+  updateTime: {color: 'grey'},
+  category: {color: 'grey'},
+});
--- screens/ToDoScreen.tsx
+++ screens/ToDoScreen.tsx
@@ -1,20 +1,45 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, SafeAreaView, ScrollView} from 'react-native';
+import TodoCard from '../Components/TodoCard';
 
 const ToDoScreen: React.FC = () => {
   return (
-    <View style={styles.container}>
-      <Text>To Do Screen</Text>
-    </View>
+    <SafeAreaView>
+      <ScrollView style={styles.Container}>
+        <View style={styles.headingContainer}>
+          <Text style={styles.headingTxt}>To-do</Text>
+          <Text style={styles.descriptionTxt}>
+            Find info related todo tasks.
+          </Text>
+        </View>
+
+        <View style={styles.bodyContainer}>
+          <TodoCard text="TOA assignment" date="June 1" category="TOA" />
+          <TodoCard text="MAD Quiz" date="June 1" category="MAD" />
+          <TodoCard text="OS Quiz" date="June 2" category="OS" />
+          <TodoCard text="FA Assignment" date="June 2" category="FA" />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
 export default ToDoScreen;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  Container: {
+    margin: 16,
+    paddingVertical: 8,
+    paddingHorizontal: 6,
   },
+  headingContainer: {},
+  headingTxt: {
+    fontSize: 24,
+    color: '#1A6B7E',
+    fontWeight: 'bold',
+  },
+  descriptionTxt: {
+    marginVertical: 12,
+  },
+  bodyContainer: {},
 });
