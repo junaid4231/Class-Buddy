058966fbb9d827c0b73ba650631041cec1158d81
Sajjad Afzaal <sajjadafzaal13416@gmail.com>
2025-06-02 07:02:24 +0500

    Conditional rendering on attendance

--- Components/AttendanceCard.tsx
+++ Components/AttendanceCard.tsx
@@ -12,15 +12,29 @@ const AttendanceCard: React.FC<AttendanceCardProps> = ({
   subtitle,
   percentage,
 }) => {
+  // Extract the numeric part from the string and convert to number
+  const numericPercentage = parseInt(percentage.replace('%', ''), 10);
+
   return (
     <View style={styles.card}>
-      <View style={styles.leftBar} />
+      <View
+        style={[
+          styles.leftBar,
+          numericPercentage >= 80 && {backgroundColor: 'green'},
+        ]}
+      />
       <View style={styles.cardContent}>
         <View style={styles.textSection}>
           <Text style={styles.title}>{title}</Text>
           <Text style={styles.subtitle}>{subtitle}</Text>
         </View>
-        <Text style={styles.percentage}>{percentage}</Text>
+        <Text
+          style={[
+            styles.percentage,
+            numericPercentage >= 80 && {color: 'green'},
+          ]}>
+          {percentage}
+        </Text>
       </View>
     </View>
   );
